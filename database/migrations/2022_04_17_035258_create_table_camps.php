523b5bbd97612374c768e2213890e5731c9aefc8
ilman09 <zidnidardiri09@gmail.com>
2022-04-20 11:31:31 +0700

    seeder

--- database/migrations/2022_04_17_035258_create_table_camps.php
+++ database/migrations/2022_04_17_035258_create_table_camps.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('camps', function (Blueprint $table) {
             $table->id();
-            $table->string('varchar',100);
+            $table->string('title',100);
             $table->string('slug',100);
             $table->integer('price')->unsigned();
             $table->timestamps();
